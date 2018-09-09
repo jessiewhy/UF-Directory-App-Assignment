eabea043e9a92b0e756ab2099547face28454e28
Your Name here <your_email@domainname.com>
2018-09-09 11:55:16 -0400

    can add code

--- listingController.js
+++ listingController.js
@@ -4,22 +4,11 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
     $scope.addListing = function() {
-      $scope.listings.push({ 
-        'code':$scope.code, 
-        'buildingName': $scope.buildingName,
-        'lat':$scope.lat,
-        'long': $scope.long,
-        'add': $scope.add});
-      $scope.code='';
-      $scope.buildingName='';
-      $scope.lat='';
-      $scope.long='';
-      $scope.add='';
+      $scope.listings.push({ 'code':$scope.code, 'building name': $scope.name, 'address':$scope.address });
+        $scope.code='';
+        $scope.name='';
+        $scope.address='';
     };
     $scope.deleteListing = function(index){
          
